48ece8a2dc64a4e02f40bc7278698e91dbb0d579
Bhaskar Das <bhaskardas090@gmail.com>
2022-05-01 20:20:39 +0530

    added styles to button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <div className="drugs">
-        <button className="drug1">Order Drug 1</button>
-        <button className="drug2">Order Drug 2</button>
+      <div className="container">
+        <div className="drugs">
+          <button className="drug1">Order Drug 1</button>
+          <button className="drug2">Order Drug 2</button>
+        </div>
+        <form className="form"></form>
       </div>
-      <form className="form"></form>
     </div>
   );
-}
+};
 
 export default App;
