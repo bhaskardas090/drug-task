c8537fbc378902044d230c106f3e5ca6d4942453
Bhaskar Das <bhaskardas090@gmail.com>
2022-05-01 20:15:05 +0530

    added button to fetch form data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="drugs">
+        <button className="drug1">Order Drug 1</button>
+        <button className="drug2">Order Drug 2</button>
+      </div>
+      <form className="form"></form>
     </div>
   );
 }
